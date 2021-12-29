d302757ed1b0c7c4e21486616891ebe0ab0f5fbe
ashkin <79046086021@yandex.ru>
2021-12-29 13:13:54 +0400

    f

--- src/Controller/HelpdeskController.php
+++ src/Controller/HelpdeskController.php
@@ -5,10 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-
+use App\Form\TicketType;
 use App\Entity\Tickets;
 
 class HelpdeskController extends AbstractController
@@ -20,12 +17,7 @@ class HelpdeskController extends AbstractController
     {
 		
 		$ticket = new Tickets();
-		$form = $this->createFormBuilder($task)
-            ->add('name', TextType::class, ['label' => 'Ваше имя:'])
-			->add('phone', TextType::class,['label' => 'Мобильный телефон:'])
-			->add('message', TextType::class,['label' => 'Сообщение:'])
-            ->add('save', SubmitType::class, ['label' => 'Create Task'])
-            ->getForm();
+		$form = $this->create(TicketType::class, $ticket);
 		
 		
         return $this->render('helpdesk/index.html.twig', [
